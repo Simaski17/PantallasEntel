39c6643c1fd703302e33ffdcdce5c4030590cf61
simaski <jimmyhernandezu@gmail.com>
2016-11-16 18:26:28 -0300

    16 noviembre 2016

---
+++ app/src/main/java/com/rinno/simaski/pantallasentel/HuaweiMateOchoDosFragment.java
@@ -0,0 +1,29 @@
+package com.rinno.simaski.pantallasentel;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HuaweiMateOchoDosFragment extends Fragment {
+
+
+    public HuaweiMateOchoDosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_huawei_mate_ocho_dos, container, false);
+    }
+
+}
--- app/src/main/java/com/rinno/simaski/pantallasentel/MainActivity.java
+++ app/src/main/java/com/rinno/simaski/pantallasentel/MainActivity.java
@@ -1,22 +1,34 @@
 package com.rinno.simaski.pantallasentel;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
 
     int contCast = 0;
+    @BindView(R.id.ivFlechaIzquierda)
+    ImageView ivFlechaIzquierda;
+    @BindView(R.id.ivFlechaDerecha)
+    ImageView ivFlechaDerecha;
+    //@BindView(R.id.pager)
+    ViewPager pager;
+    @BindView(R.id.btIrCatalogo)
+    Button btIrCatalogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +47,33 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
-
-        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+        pager = (ViewPager) findViewById(R.id.pager);
         ViewPagerIndicator indicator = (ViewPagerIndicator) findViewById(R.id.pager_indicator);
         pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
 
         indicator.setPager(pager);
 
 
-
     }
+
+    @OnClick({R.id.ivFlechaIzquierda, R.id.ivFlechaDerecha, R.id.btIrCatalogo})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ivFlechaIzquierda:
+                Toast.makeText(getApplication(), "Flecha", Toast.LENGTH_SHORT).show();
+                pager.setCurrentItem(pager.getCurrentItem() - 1, true); //getItem(-1) for previous
+                break;
+            case R.id.ivFlechaDerecha:
+                Toast.makeText(getApplication(), "Flecha derecha", Toast.LENGTH_SHORT).show();
+                pager.setCurrentItem(pager.getCurrentItem() + 1, true); //getItem(-1) for previous
+                break;
+            case R.id.btIrCatalogo:
+                Toast.makeText(getApplication(), "Flecha", Toast.LENGTH_SHORT).show();
+                //pager.setCurrentItem(pager.getCurrentItem() + 1, true); //getItem(-1) for previous
+                break;
+        }
+    }
+
     private class MyPagerAdapter extends FragmentPagerAdapter {
 
         public MyPagerAdapter(FragmentManager fm) {
@@ -53,20 +82,24 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int pos) {
-            switch(pos) {
+            switch (pos) {
 
                 case 0:
                     return new HuaweiPNueveTresFragment();
                 case 1:
                     return new HuaweiPNueveUnoFragment();
                 case 2:
-                    return new HuaweiPNueveDosFragment();
+                    return new SamsungSSieteUnoFragment();
                 case 3:
-                    return new LgGCincoUnoFragment();
+                    return new SamsungSSieteDosFragment();
                 case 4:
-                    return new LgGCincoDosFragment();
+                    return new SamsungSSieteTresFragment();
                 case 5:
-                    return new LgGCincoTresFragment();
+                    return new SamsungSSieteEdgeUnoFragment();
+                case 6:
+                    return new SamsungSSieteEdgeDosFragment();
+                case 7:
+                    return new SamsungSSieteEdgeTresFragment();
                 default:
                     return new HuaweiPNueveTresFragment();
             }
@@ -74,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 6;
+            return 8;
         }
     }
 }
